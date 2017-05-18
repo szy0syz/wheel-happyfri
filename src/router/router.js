cca4449b44519b92badfa8363d1ef0dd2734a2ec
szy0syz <szy0syz@gmail.com>
2017-05-18 23:23:17 +0800

    init Vue & Router
    初始化Vue和Vue-Router

---
+++ src/router/router.js
@@ -0,0 +1,23 @@
+/**
+ * Created by jerry on 2017/5/18.
+ */
+import App from '../App'
+
+export default [{
+  path: '/',
+  component: App,
+  children: [{
+    path: '',
+    component: r => require.ensure([], () => r(require('../page/home')), 'home')
+  }, {
+    path: 'item',
+    component: function (r) {
+      return require.ensure([], function () {
+        return r(require('../page/item'))
+      }, 'item')
+    }
+  }, {
+    path: '/score',
+    component: r => require.ensure([], () => r(require('../page/score')), 'score')
+  }]
+}]
