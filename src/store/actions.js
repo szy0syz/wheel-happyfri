c794c9a3013b66792b48ed5b8f8160bb982b37e6
szy0syz <szy0syz@gmail.com>
2017-05-19 16:41:27 +0800

    + 新增state.actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,3 +1,24 @@
 /**
  * Created by jerry on 2017/5/19.
  */
+import ajax from '../config/ajax'
+
+export default {
+  addNum ({commit, state}, id) {
+    commit('REMBER_ANSWER', { id })
+    if (state.itemNum < state.itemDetail.length) {
+      commit('ADD_ITEMNUM', {
+        num: 1
+      })
+    }
+  },
+  getDate ({commit, state}) {
+    ajax('GET', 'http://operating-activities.putao.com/happyfriday?active_topic_id=4')
+      .then(res => {
+        commit('GET_DATA', { res })
+      })
+  },
+  initializeData ({commit}) {
+    commit('INITIALIZE_DATA')
+  }
+}
