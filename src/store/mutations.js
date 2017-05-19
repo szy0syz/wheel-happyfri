c3aa367a2e94816f6b00ed5fd47a2e636deb99db
szy0syz <szy0syz@gmail.com>
2017-05-19 15:34:39 +0800

    解决WebStorm报错问题

---
+++ src/store/mutations.js
@@ -0,0 +1,35 @@
+/**
+ * Created by jerry on 2017/5/19.
+ */
+/**
+ * Created by jerry on 2017/5/18.
+ */
+const GET_DATA = 'GET_DATA'
+const ADD_ITEMNUM = 'ADD_ITEMNUM'
+const REMBER_ANSWER = 'REMBER_ANSWER'
+const REMBER_TIME = 'REMBER_TIME'
+const INITIALIZE_DATA = 'INITIALIZE_DATA'
+const GET_USER_INFORM = 'GET_USER_INFORM'
+
+export default {
+  [GET_DATA] (state, payload) {
+    if (payload.res.httpStatusCode == 200) {
+      state.itemDetail = payload.res.topiclist
+    }
+  },
+  [ADD_ITEMNUM] (state, payload) {
+    state.user_id = payload.res.users_id // user_id 这个属性state没提前定义
+  },
+  [REMBER_ANSWER] (state, payload) {
+    state.itemNum += payload.num
+  },
+  [REMBER_TIME] (state) {
+    state.timer = setInterval(() => {
+      state.alltime++
+    }, 1000)
+  },
+  [INITIALIZE_DATA] (state) {
+    state.itemNum = 1
+    state.allTime = 0
+  }
+}
